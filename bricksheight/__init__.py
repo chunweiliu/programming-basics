eb16963aa81b1251fc1976f63e147378733d9e7a
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-11-04 23:18:28 -0800

    Add unittest

--- bricksheight/__init__.py
+++ bricksheight/__init__.py
@@ -2,45 +2,44 @@
 Brick's Height
 
 
-ooxxkkkkkkkssss-- (bird view)
+ovvnnnnbbx-- (bird view)
 
-    kkkkkkk
-          sssss
-  xxx     sssss
-  xxx     sssss
-oooovvv   sssss
-oooovvv   sssss
------------------> float
-012345678901234
+   nnnn
+      bbb
+ vvvv bbb
+oooo   xxx
+------------> float
+0123456789
 """
 
 from collections import namedtuple
+import unittest
 
-Region = namedtuple('Region', ['start', 'end', 'height'])
+Brick = namedtuple('Brick', ['start', 'end', 'height'])
 
 
 class Bricks(object):
     def __init__(self):
-        self.bird_view = []  # List of regions
+        self.bird_view = []  # List of bricks
 
-    def drop(self, region):
+    def drop(self, brick):
         if not self.bird_view:
-            self.bird_view.append(region)
+            self.bird_view.append(brick)
         else:
             # No need to seperate regions. Just overlap different regions.
             overlap = False
             for start, end, height in self.bird_view:
-                if max(start, region.start) <= min(end, region.end):  # !
+                if max(start, brick.start) <= min(end, brick.end):  # !
                     overlap = True
                     self.bird_view.append(
-                        Region(region.start, region.end,
-                               height + region.height))
+                        Brick(brick.start, brick.end,
+                              height + brick.height))
                     break
 
             if not overlap:  # !
-                self.bird_view.append(region)
+                self.bird_view.append(brick)
 
-        self.bird_view.sort(key=lambda region: region[2], reverse=True)  # !
+        self.bird_view.sort(key=lambda brick: brick[2], reverse=True)  # !
 
     def height(self, point):
         for start, end, height in self.bird_view:
@@ -48,13 +47,38 @@ class Bricks(object):
                 return height
         return 0
 
+
+class TestBricks(unittest.TestCase):
+    def test_initial(self):
+        bricks = Bricks()
+        for i in range(10):
+            self.assertEqual(bricks.height(i), 0)
+
+    def test_one_brick(self):
+        bird_view = [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+        bricks = Bricks()
+        bricks.drop(Brick(0, 3, 1))
+        for i in range(10):
+            self.assertEqual(bricks.height(i), bird_view[i])
+
+    def test_two_bricks(self):
+        bird_view = [1, 1, 1, 1, 0, 0, 0, 1, 1, 1]
+        bricks = Bricks()
+        bricks.drop(Brick(0, 3, 1))
+        bricks.drop(Brick(7, 9, 1))
+        for i in range(10):
+            self.assertEqual(bricks.height(i), bird_view[i])
+
+    def test_bricks(self):
+        bird_view = [1, 2, 2, 4, 4, 4, 4, 3, 3, 1]
+        bricks = Bricks()
+        bricks.drop(Brick(0, 3, 1))
+        bricks.drop(Brick(7, 9, 1))
+        bricks.drop(Brick(1, 4, 1))
+        bricks.drop(Brick(6, 8, 2))
+        bricks.drop(Brick(3, 6, 1))
+        for i in range(10):
+            self.assertEqual(bricks.height(i), bird_view[i])
+
 if __name__ == '__main__':
-    bricks = Bricks()
-    bricks.drop(Region(0, 3, 2))
-    bricks.drop(Region(4, 6, 2))
-    bricks.drop(Region(10, 14, 5))
-    bricks.drop(Region(2, 4, 2))
-    bricks.drop(Region(4, 10, 1))
-    print bricks.bird_view
-    for i in range(15):
-        print bricks.height(i)
+    unittest.main()
