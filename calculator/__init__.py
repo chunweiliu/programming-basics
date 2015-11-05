8bd974d5bb05222efed6a0f3f07bedd24590bda0
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-11-05 10:45:07 -0800

    Add unittest.

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,74 +1,108 @@
 """
-An implementation a simple calculator using a BNF parser:
+Calculator
 
 <expr> ::= <expr> + <term>
-       |   <expr> - <term>
-       |   <term>
+         | <expr> - <term>
+         | <term>
 
-<term> ::= <factor> * <term>
-       |   <factor> / <term>
-       |   <factor>
+<term> ::= <term> * <factor>
+         | <term> / <factor>
+         | <factor>
 
-<factor> ::= 0|1|2|3|4|5|6|7|8|9
-         |   (<expr>)
+<factor> ::= [\d]+
+           | (<expr>)
 
 """
 import re
+import unittest
 
 
-class Calculator():
+class Calculator(object):
     def __init__(self):
         self.tokens = []
         self.current_token = None
 
     def evaluate(self, expression):
-        # Regular expression for matching numbers (-2, 10, 1.5) and operator.
-        self.tokens = re.findall('[-]?[\d.]+|[()+*-/]', expression)
+        # Regular expression is a key. To handle negative and multiple digits,
+        # use '[-]?\d*\.*\d+'.
+        self.tokens = re.findall('[-]?\d*\.*\d+|[()+*-/]', expression)
         self.current_token = self.tokens[0] if self.tokens else None
-        return self.expr()
+        return self._expr()
 
-    def expr(self):
-        result = self.term()
-        # Re-evaluate the current token utill not hitting the '+' or '-'.
-        # E.g 1 + 2 + 3
-        #           ^ current_token is here after parsing the first two.
-        while self.current_token in ['+', '-']:
-            if self.current_token == '+':
-                self.advance()
-                result += self.term()
-            if self.current_token == '-':
-                self.advance()
-                result -= self.term()
-        return result
-
-    def term(self):
-        result = self.factor()
-        while self.current_token in ['*', '/']:
-            if self.current_token == '*':
-                self.advance()
-                result *= self.factor()
-            if self.current_token == '/':
-                self.advance()
-                result /= self.factor()
-        return result
-
-    def factor(self):
-        if self.current_token[0] == '(':
-            self.advance()
-            result = self.expr()
-            self.advance()
-            return result
-
-        result = float(self.current_token)
-        self.advance()
-        return result
-
-    def advance(self):
+    def _advance(self):
         self.tokens = self.tokens[1:]
         self.current_token = self.tokens[0] if self.tokens else None
 
+    def _expr(self):
+        result = self._term()  # Process * and / first.
+        while self.current_token in ['+', '-']:
+            if self.current_token == '+':
+                self._advance()  # Advance the operator.
+                result += self._term()
+            else:
+                self._advance()
+                result -= self._term()
+        return result
+
+    def _term(self):
+        result = self._factor()
+        while self.current_token in ['*', '/']:
+            if self.current_token == '*':
+                self._advance()
+                result *= self._factor()
+            else:
+                self._advance()
+                result /= self._factor()
+        return result
+
+    def _factor(self):
+        if self.current_token == '(':
+            self._advance()
+            result = self._expr()
+            self._advance()
+            return result
+
+        result = float(self.current_token)
+        self._advance()  # Advance the number.
+        return result
+
+
+class TestCalculator(unittest.TestCase):
+    def test_zero(self):
+        expression = '0'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
+    def test_plus(self):
+        expression = '1 + 1'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
+    def test_order(self):
+        expression = '1 + 2 * 3'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
+    def test_parenthesis(self):
+        expression = '2 * (2 + 3)'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
+    def test_float(self):
+        expression = '2.2 * (2 + 3)'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
+    def test_two_digits(self):
+        expression = '22 * (2 + 3)'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
+    def test_negative_digit(self):
+        expression = '-2 * (2 + 3)'
+        calculator = Calculator()
+        self.assertEqual(eval(expression), calculator.evaluate(expression))
+
 
 if __name__ == '__main__':
-    calculator = Calculator()
-    expression = '(1 + 2) + 3'
-    print calculator.evaluate(expression)
+    unittest.main()
