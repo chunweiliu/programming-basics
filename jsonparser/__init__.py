155c7ce6ad3934e295cbfe4f680985cb564fafd0
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-11-05 20:19:56 -0800

    Add unittest

--- jsonparser/__init__.py
+++ jsonparser/__init__.py
@@ -1,82 +1,103 @@
+"""
+JSON Parser
+
+Implement a JSON parser for the following data type.
+
+JSON        Python
+----------------------
+array       list
+object      dict
+----------------------
+"""
 import re
+import unittest
 
 
 class JSONParser(object):
     def __init__(self):
-        self.tokens = []  # Use global variable for parsing.
+        self.tokens = []
 
-    def parse(self, text):
-        self.tokens = re.findall('[\d.\d]+|[\w]+|[\[\]\{\}]', text)
-        if self.tokens[0] == '[':
-            return self._parse_array(self.tokens)
-        if self.tokens[0] == '{':
-            return self._parse_object(self.tokens)
-        return None
+    def parse(self, string):
+        self.tokens = re.findall(r'[-]?\d*\.?\d+|[\[\]{}]|\w+', string)
+        self.current_token = self.tokens[0] if self.tokens else None
 
-    def _parse_array(self, tokens):
-        tokens.pop(0)  # Pop the '['
+        # Assume the input string is valid.
+        if self.current_token == '[':
+            return self._parse_array()
+        if self.current_token == '{':
+            return self._parse_object()
+
+    def _advance(self):
+        self.tokens.pop(0)
+        self.current_token = self.tokens[0] if self.tokens else None
+
+    def _parse_array(self):
+        self._advance()  # advance from [
 
         array = []
-        while tokens and tokens[0] != ']':
-            if tokens[0] == '[':
-                array.append(self._parse_array(tokens))
-            elif tokens[0] == '{':
-                array.append(self._parse_object(tokens))
+        while self.current_token and self.current_token != ']':
+            if self.current_token == '[':
+                array.append(self._parse_array())
+            elif self.current_token == '{':
+                array.append(self._parse_object())
             else:
-                array.append(tokens[0])
-                tokens.pop(0)
+                array.append(float(self.current_token))
+                self._advance()
 
-        tokens.pop(0)  # Pop the ']'
+        self._advance()  # advance from ]
         return array
 
-    def _parse_object(self, tokens):
-        tokens.pop(0)  # Pop the '{'
+    def _parse_object(self):
+        self._advance()  # advance from {
 
         json_object = {}
-        while tokens and tokens[0] != '}':
-            key = tokens[0]
-            tokens.pop(0)
+        while self.current_token and self.current_token != '}':
+            key, value = self.current_token, self.tokens[1]
 
-            if tokens[0] in ['[', '{']:
-                json_object[key] = self._parse_array(tokens) \
-                    if tokens[0] == '[' else self._parse_object(tokens)
+            # Need to pop the key first, so the rest follows the pattern.
+            self._advance()
+
+            if value == '[':
+                json_object[key] = self._parse_array()
+            elif value == '{':
+                json_object[key] = self._parse_object()
             else:
-                json_object[key] = tokens[0]
-                tokens.pop(0)
+                json_object[key] = float(value)
+                self._advance()
 
-        tokens.pop(0)  # Pop the '}'
+        self._advance()  # advance from }
         return json_object
 
+
+class TestJSONParser(unittest.TestCase):
+    def test_array(self):
+        string = '[-1, .1, 1, 2, 2.5, 10]'
+        expected = [-1, .1, 1, 2, 2.5, 10]
+
+        json_parser = JSONParser()
+        self.assertEqual(json_parser.parse(string), expected)
+
+    def test_array_of_array(self):
+        string = '[[-1, .1, 1], [2, 2.5, 10]]'
+        expected = [[-1, .1, 1], [2, 2.5, 10]]
+
+        json_parser = JSONParser()
+        self.assertEqual(json_parser.parse(string), expected)
+
+    def test_object(self):
+        string = '{\'key1\': 10, \'key2\': 20}'
+        expected = {'key1': 10, 'key2': 20}
+
+        json_parser = JSONParser()
+        self.assertEqual(json_parser.parse(string), expected)
+
+    def test_object_of_object(self):
+        string = '{\'key1\': {\'key1key\': 10}, \'key2\': [20, 21, 22]}'
+        expected = {'key1': {'key1key': 10}, 'key2': [20, 21, 22]}
+
+        json_parser = JSONParser()
+        self.assertEqual(json_parser.parse(string), expected)
+
+
 if __name__ == '__main__':
-    json_parser = JSONParser()
-
-    print "First Step"
-    for value in json_parser.parse(" [ 10, 20, 30.1 ] "):
-        print value
-
-    print "\nSecond Step"
-    for value in json_parser.parse(" [ 10 , 20, \"hello\", 30.1 ] "):
-        print value
-
-    print "\nThird Step"
-    for key, value in json_parser.parse("""{
-            "hello": "world",
-            "key1": 20,
-            "key2": 20.3,
-            "foo": "bar" }""").items():
-        print key, value
-
-    print "\nFourth Step"
-    for key, value in json_parser.parse("""{
-            "hello": "world",
-            "key1": 20,
-            "key2": 20.3,
-            "foo": {
-                "hello1": "world1",
-                "key3": [200, 300, {'key4': 400}]
-            } }""").items():
-        if isinstance(value, dict):
-            for key2, value2 in value.items():
-                print key2, value2
-        else:
-            print key, value
+    unittest.main()
