de096f6e3ad7d552c4e98b5ff8e5d55f6caa6bf8
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-11-05 20:47:05 -0800

    Add unittest

--- calendar/__init__.py
+++ calendar/__init__.py
@@ -1,102 +1,144 @@
-#!/usr/bin/env python
+"""
+Calendar
 
-from datetime import datetime
+Implement a Calendar class which supports two operations:
+
+1. Schedule events
+
+2. Show the organized schedule
+
+
+|aaaaaaaaaa|
+     |bbbbbbbbbb|
+  |cccccc|
+-------------------
+|a|ac|abc|a|b   | -> Output
+          b
+
+"""
+
 from collections import namedtuple
+from datetime import datetime
 
-import sys
+import unittest
 
-Event = namedtuple('Event', ['id', 'start_time', 'end_time'])
-ConflictedTimeWindow = \
-    namedtuple('ConflictedTimeWindow',
-               ['start_time', 'end_time', 'conflicted_event_ids'])
+Event = namedtuple('Event', ['name', 'start', 'end'])
+TimeWindow = namedtuple('TimeWindow', ['start', 'end', 'events'])
 
 
 class Calendar(object):
-    # Should allow multiple events to be scheduled over the same time window.
     def __init__(self):
-        self.timestamp = []
-        self.events = []
+        self.timestamps = []  # list of time
+        self.events = []  # list of events
+        self._schedule = []  # list of TimeWindow
 
-    def schedule(self, event):
-        # How to devide intervals?
-        for timestamp in [event.start_time, event.end_time]:
-            if timestamp not in self.timestamp:
-                self.timestamp.append(timestamp)
-
+    def add_event(self, event):
+        # Update events
         self.events.append(event)
 
-    def find_conflicted_time_windows(self):
-        # Divide all timestamps. For every regions, check every events. O(n^2)
-        self.timestamp.sort()
+        # Update timestamps
+        for timestamp in [event.start, event.end]:
+            if timestamp not in self.timestamps:
+                self.timestamps.append(timestamp)
+        self.timestamps.sort()
 
-        conflicted_events = []
-        for start_time, end_time in zip(self.timestamp[:-1],
-                                        self.timestamp[1:]):
-            conflicted_ids = []
+        # Update schedule
+        self._schedule = []
+        for start, end in zip(self.timestamps[:-1], self.timestamps[1:]):
+            events = []
             for event in self.events:
-                # Region overlaping
-                if (max(start_time, event.start_time) <
-                        min(end_time, event.end_time)):
-                    conflicted_ids.append(event.id)
-            if len(conflicted_ids) > 1:
-                conflicted_events.append(
-                    ConflictedTimeWindow(start_time, end_time, conflicted_ids))
-        return conflicted_events
+                if max(event.start, start) < min(event.end, end):
+                    events.append(event.name)
+            self._schedule.append(TimeWindow(start, end, events))
+
+    def schedule(self):
+        return self._schedule
 
 
-def main(argv):
-    calendar = Calendar()
-    calendar.schedule(Event(1,
-        datetime.strptime('2014-01-01 10:00', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-01 11:00', '%Y-%m-%d %H:%M')))
-    calendar.schedule(Event(2,
-        datetime.strptime('2014-01-01 11:00', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-01 12:00', '%Y-%m-%d %H:%M')))
-    calendar.schedule(Event(3,
-        datetime.strptime('2014-01-01 12:00', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-01 13:00', '%Y-%m-%d %H:%M')))
+class TestCalendar(unittest.TestCase):
+    def test_no_overlap(self):
+        calendar = Calendar()
 
-    calendar.schedule(Event(4,
-        datetime.strptime('2014-01-02 10:00', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-02 11:00', '%Y-%m-%d %H:%M')))
-    calendar.schedule(Event(5,
-        datetime.strptime('2014-01-02 09:30', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-02 11:30', '%Y-%m-%d %H:%M')))
-    calendar.schedule(Event(6,
-        datetime.strptime('2014-01-02 08:30', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-02 12:30', '%Y-%m-%d %H:%M')))
+        event_name = 'a'
+        start_time = datetime.strptime('2014-01-01 10:00', '%Y-%m-%d %H:%M')
+        end_time = datetime.strptime('2014-01-01 11:00', '%Y-%m-%d %H:%M')
 
-    calendar.schedule(Event(7,
-        datetime.strptime('2014-01-03 10:00', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-03 11:00', '%Y-%m-%d %H:%M')))
-    calendar.schedule(Event(8,
-        datetime.strptime('2014-01-03 09:30', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-03 10:30', '%Y-%m-%d %H:%M')))
-    calendar.schedule(Event(9,
-        datetime.strptime('2014-01-03 09:45', '%Y-%m-%d %H:%M'),
-        datetime.strptime('2014-01-03 10:45', '%Y-%m-%d %H:%M')))
+        calendar.add_event(Event(event_name, start_time, end_time))
+        expected_schedule = [TimeWindow(start_time, end_time, [event_name])]
 
-    print calendar.find_conflicted_time_windows()
-    # Should print something like the following
+        self.assertEqual(expected_schedule, calendar.schedule())
 
-    # [ConflictedTimeWindow(start_time=datetime.datetime(2014, 1, 2, 9, 30),
-    #                       end_time=datetime.datetime(2014, 1, 2, 10, 0),
-    #                       conflicted_event_ids=[5, 6]),
-    #  ConflictedTimeWindow(start_time=datetime.datetime(2014, 1, 2, 10, 0),
-    #                       end_time=datetime.datetime(2014, 1, 2, 11, 0),
-    #                       conflicted_event_ids=[4, 5, 6]),
-    #  ConflictedTimeWindow(start_time=datetime.datetime(2014, 1, 2, 11, 0),
-    #                       end_time=datetime.datetime(2014, 1, 2, 11, 30),
-    #                       conflicted_event_ids=[5, 6]),
-    #  ConflictedTimeWindow(start_time=datetime.datetime(2014, 1, 3, 9, 45),
-    #                       end_time=datetime.datetime(2014, 1, 3, 10, 0),
-    #                       conflicted_event_ids=[8, 9]),
-    #  ConflictedTimeWindow(start_time=datetime.datetime(2014, 1, 3, 10, 0),
-    #                       end_time=datetime.datetime(2014, 1, 3, 10, 30),
-    #                       conflicted_event_ids=[7, 8, 9]),
-    #  ConflictedTimeWindow(start_time=datetime.datetime(2014, 1, 3, 10, 30),
-    #                       end_time=datetime.datetime(2014, 1, 3, 10, 45),
-    #                       conflicted_event_ids=[7, 9])]
+    def test_overlap(self):
+        calendar = Calendar()
+
+        calendar.add_event(
+            Event('a',
+                  datetime.strptime('2014-01-01 10:00', '%Y-%m-%d %H:%M'),
+                  datetime.strptime('2014-01-01 11:00', '%Y-%m-%d %H:%M')))
+
+        calendar.add_event(
+            Event('b',
+                  datetime.strptime('2014-01-01 10:30', '%Y-%m-%d %H:%M'),
+                  datetime.strptime('2014-01-01 11:30', '%Y-%m-%d %H:%M')))
+
+        expected_schedule = [
+            TimeWindow(datetime.strptime('2014-01-01 10:00',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 10:30',
+                                         '%Y-%m-%d %H:%M'), ['a']),
+            TimeWindow(datetime.strptime('2014-01-01 10:30',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 11:00',
+                                         '%Y-%m-%d %H:%M'), ['a', 'b']),
+            TimeWindow(datetime.strptime('2014-01-01 11:00',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 11:30',
+                                         '%Y-%m-%d %H:%M'), ['b'])]
+
+        self.assertEqual(expected_schedule, calendar.schedule())
+
+    def test_overlap2(self):
+        calendar = Calendar()
+
+        calendar.add_event(
+            Event('a',
+                  datetime.strptime('2014-01-01 10:00', '%Y-%m-%d %H:%M'),
+                  datetime.strptime('2014-01-01 11:00', '%Y-%m-%d %H:%M')))
+
+        calendar.add_event(
+            Event('b',
+                  datetime.strptime('2014-01-01 10:30', '%Y-%m-%d %H:%M'),
+                  datetime.strptime('2014-01-01 11:30', '%Y-%m-%d %H:%M')))
+
+        calendar.add_event(
+            Event('c',
+                  datetime.strptime('2014-01-01 10:15', '%Y-%m-%d %H:%M'),
+                  datetime.strptime('2014-01-01 10:45', '%Y-%m-%d %H:%M')))
+
+        expected_schedule = [
+            TimeWindow(datetime.strptime('2014-01-01 10:00',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 10:15',
+                                         '%Y-%m-%d %H:%M'), ['a']),
+            TimeWindow(datetime.strptime('2014-01-01 10:15',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 10:30',
+                                         '%Y-%m-%d %H:%M'), ['a', 'c']),
+            TimeWindow(datetime.strptime('2014-01-01 10:30',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 10:45',
+                                         '%Y-%m-%d %H:%M'), ['a', 'b', 'c']),
+            TimeWindow(datetime.strptime('2014-01-01 10:45',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 11:00',
+                                         '%Y-%m-%d %H:%M'), ['a', 'b']),
+            TimeWindow(datetime.strptime('2014-01-01 11:00',
+                                         '%Y-%m-%d %H:%M'),
+                       datetime.strptime('2014-01-01 11:30',
+                                         '%Y-%m-%d %H:%M'), ['b'])]
+
+        self.assertEqual(expected_schedule, calendar.schedule())
+
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    unittest.main()
